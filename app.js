6637aff6205649a9f6dfa1066d34d7a93d3e8f73
Shashi <shashikunal@gmail.com>
2018-11-26 12:29:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-var myApp = angular.module('testApp' , []);
+var myApp = angular.module("myApp" , []);
 
-myApp.controller('myCtr' , function($scope , $http){
-    $http.get("./data.json").then(function(x){
-    $scope.users = x.data;
-    });
-});
+myApp.controller("myCtr" , ($scope)=>{
 
+   $scope.remove = function(user){
+       var removedItem = $scope.users.indexOf(user);
+       $scope.users.splice(removedItem , 1);
+   };
 
+$scope.addusers = function(){
+    $scope.users.push({
+        name:$scope.newUser.name,
+        age : $scope.newUser.age,
+        company:$scope.newUser.company
+    })
+}
+
+    $scope.users = [
+        {
+            name:"shashi",
+            age : 30 , 
+            company:"testyantra"
+        },
+
+        {
+            name:"vinay",
+            age : 40 , 
+            company:"qspiders"  
+        },
+        {
+            name:"manu",
+            age : 40 , 
+            company:"qspiders"  
+        },
+    ]
+})
